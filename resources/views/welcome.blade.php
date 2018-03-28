2b0ca0d640dbc5db912f383b1ef9abd14f2d521a
Kalugy <danieltrujillo901@gmail.com>
2018-03-28 17:36:27 -0500

    PRIMER COMMIT DESDE LARAVEL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    xDXDXDXDHOLA
                 </div>
 
                 <div class="links">
